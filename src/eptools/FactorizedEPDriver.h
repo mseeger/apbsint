cacfb26c87162a2bd1d52c0b43a6b58c6acb9013
Matthias Seeger <mseeger@gmail.com>
2014-03-10 16:05:36 +0100

    Nothing much

--- src/eptools/FactorizedEPDriver.h
+++ src/eptools/FactorizedEPDriver.h
@@ -14,7 +14,7 @@
 #endif
 
 #include "src/eptools/potentials/PotentialManager.h"
-#include "src/eptools/FactEPRepresBivarPrec.h"
+#include "src/eptools/FactorizedEPRepresentation.h"
 #include "src/eptools/FactEPMaximumPiValues.h"
 #include "src/eptools/FactEPMaximumAValues.h"
 #include "src/eptools/FactEPMaximumCValues.h"
@@ -28,36 +28,24 @@
    * - Univariate potentials, inference over x. All potentials must be
    *   in argument group 'atypeUnivariate'. Representation of type
    *   'FactorizedEPRepresentation'
-   * - Bivariate precision potentials, inference over x and tau. All
-   *   potentials must be in argument group 'atypeBivarPrec'. Representation
-   *   of type 'FactEPRepresBivarPrec'
+   * - Bivariate precision potentials are present, inference over x and
+   *   tau. Some potentials must be in group 'atypeBivarPrec'. Potentials
+   *   in group 'atypeUnivariate' (if any) are followed by such in group
+   *   'atypeBivarPrec'
    * <p>
-   * Univariate potentials:
-   *   p(x) = Z^-1 prod_j t_j(s_j),  s = B x, x[0:n-1], s[0:m-1]
-   * {t_j(s_j)} given by 'epPots' (argument group 'atypeUnivariate'). B,
-   * message parameters beta, pi maintained in 'epRepr'
-   * ('FactorizedEPRepresentation').
-   * Marginal moments represented by [beta_i], [pi_i] ('margBeta',
-   * 'margPi').
+   * The potential manager 'epPots' consists of univariate potentials
+   * t_j(s_j), followed by (optionally) bivariate precision potentials
+   * t_j(s_j,tau_k(j)). The B factor and beta, pi message parameters are
+   * maintained by 'epRepr'. With precision potentials, 'epRepr' also
+   * maintains the k(j) index and a, c (Gamma) message parameters.
+   *
+   * Marginal moments w.r.t. x [beta_i], [pi_i] in 'margBeta', 'margPi'.
    * Relationship between marginals and message parameters:
    *   pi_i = sum_j pi_ji,   beta_i = sum_j beta_ji
-   * NOTE: A model may consist of several drivers and representations. In
-   * particular, if it contains both univariate and bivariate potentials, two
-   * drivers are involved. pi_i, beta_i are sums over message parameters of
-   * all parts.
-   * <p>
-   * Bivariate potentials over precision parameters:
-   *   p(x,tau) = Z^-1 prod_j t_j(s_j,tau_k(j)),  s = B x,
-   *              x[0:n-1], s[0:m-1], tau[0:K-1]
-   * {t_j(.)} given by 'epPots' (argument group 'atypeBivarPrec'). B, [k(j)],
-   * message parameters beta, pi, a, c maintained in 'epRepr'
-   * ('FactEPRepresBivarPrec').
-   * Marginal moments represented by [beta_i], [pi_i], [a_k], [c_k]
-   * ('margBeta', 'margPi', 'margA', 'margC').
-   * Relationship between marginals and message parameters:
+   * With precision potentials: Marginal moments w.r.t. tau [a_k], [c_k] in
+   * 'margA', 'margC'. Relationship between marginals and message
+   * parameters:
    *   a_k = sum_j a_jk,   c_k = sum_j c_jk
-   * NOTE: Even if the model consists of several drivers and representations,
-   * only one of them can maintain potentials over tau.
    * <p>
    * Sequential EP update:
    * Main service 'sequentialUpdate'. Involves computing cavity marginals,
@@ -108,7 +96,6 @@
 
     Handle<PotentialManager> epPots;       // Potential manager
     Handle<FactorizedEPRepresentation> epRepr;
-    FactEPRepresBivarPrec* epReprPrec;     // Only for bivar. prec. pots.
     ArrayHandle<double> margBeta,margPi;
     double piMinThres;
     Handle<FactEPMaximumPiValues> epMaxPi; // Selective damping (optional)
@@ -139,7 +126,8 @@
 		       const Handle<FactEPMaximumPiValues>& pepMaxPi=
 		       HandleZero<FactEPMaximumPiValues>::get()) :
       epPots(pepPots),epRepr(pepRepr),epReprPrec(0),margBeta(pmargBeta),
-      margPi(pmargPi),piMinThres(ppiMinThres),epMaxPi(pepMaxPi) {
+      margPi(pmargPi),piMinThres(ppiMinThres),epMaxPi(pepMaxPi),aMinThres(0.0),
+      cMinThres(0.0) {
       int numN=pepRepr->numVariables();
 
       if (ppiMinThres<=0.0 || pmargBeta.size()!=numN || pmargPi.size()!=numN)
@@ -150,9 +138,8 @@
     }
 
     /**
-     * Constructor (bivariate precision potentials). All potentials in
-     * 'pepPots' must be in argument group
-     * 'EPScalarPotential::atypeBivarPrec'.
+     * Constructor (bivariate precision potentials). The number of bivar.
+     * prec. potentials in 'pepPots' must be the same as in 'pepRepr'.
      *
      * @param pepPots
      * @param pepRepr
@@ -168,7 +155,7 @@
      * @param pepMaxC     "
      */
     FactorizedEPDriver(const Handle<PotentialManager>& pepPots,
-		       const Handle<FactEPRepresBivarPrec>& pepRepr,
+		       const Handle<FactorizedEPRepresentation>& pepRepr,
 		       const ArrayHandle<double>& pmargBeta,
 		       const ArrayHandle<double>& pmargPi,
 		       const ArrayHandle<double>& pmargA,
@@ -180,19 +167,20 @@
 		       HandleZero<FactEPMaximumAValues>::get(),
 		       const Handle<FactEPMaximumCValues>& pepMaxC=
 		       HandleZero<FactEPMaximumCValues>::get()) :
-      epPots(pepPots),epRepr(pepRepr),epReprPrec(pepRepr.p()),
-      margBeta(pmargBeta),margPi(pmargPi),margA(pmargA),margC(pmargC),
-      piMinThres(ppiMinThres),aMinThres(paMinThres),cMinThres(pcMinThres),
-      epMaxPi(pepMaxPi),epMaxA(pepMaxA),epMaxC(pepMaxC) {
-      int numN=pepRepr->numVariables(),numK=pepRepr->numPrecVars();
+      epPots(pepPots),epRepr(pepRepr),margBeta(pmargBeta),margPi(pmargPi),
+      margA(pmargA),margC(pmargC),piMinThres(ppiMinThres),
+      aMinThres(paMinThres),cMinThres(pcMinThres),epMaxPi(pepMaxPi),
+      epMaxA(pepMaxA),epMaxC(pepMaxC) {
+      int numN=pepRepr->numVariables(),numK=pepRepr->numPrecVariables();
 
-      if (ppiMinThres<=0.0 || pmargBeta.size()!=numN || pmargPi.size()!=numN ||
-	  paMinThres<=0.0 || pcMinThres<=0.0 || pmargA.size()!=numK ||
-	  pmargC.size()!=numK)
+      if (ppiMinThres<=0.0 || numK<=0 || pmargBeta.size()!=numN ||
+	  pmargPi.size()!=numN || paMinThres<=0.0 || pcMinThres<=0.0 ||
+	  pmargA.size()!=numK || pmargC.size()!=numK)
 	throw InvalidParameterException(EXCEPT_MSG(""));
-      if (pepPots->size()!=
-	  pepPots->numArgumentGroup(EPScalarPotential::atypeBivarPrec))
-	throw InvalidParameterException(EXCEPT_MSG("Potentials must be in group 'atypeBivarPrec'"));
+      if (pepRepr->numBVPrecPotentials()!=
+	  pepPots->numArgumentGroup(EPScalarPotential::atypeBivarPrec) ||
+	  pepRepr->numBVPrecPotentials()==0)
+	throw InvalidParameterException(EXCEPT_MSG("Some potentials must be in group 'atypeBivarPrec'"));
     }
 
     virtual ~FactorizedEPDriver() {}
@@ -205,10 +193,11 @@
       return epRepr->numPotentials();
     }
 
-    virtual int numPrecVars() const {
-      if (epReprPrec==0)
+    virtual int numPrecVariables) const {
+      int numK=epRepr->numPrecVariables();
+      if (numK==0)
 	throw WrongStatusException(EXCEPT_MSG(""));
-      return epReprPrec->numPrecVars();
+      return numK;
     }
 
     virtual const PotentialManager& getEPPotentials() const {
@@ -224,13 +213,13 @@
     }
 
     virtual const ArrayHandle<double>& getMarginalsA() const {
-      if (epReprPrec==0)
+      if (epRepr->numPrecVariables()==0)
 	throw WrongStatusException(EXCEPT_MSG(""));
       return margA;
     }
 
     virtual const ArrayHandle<double>& getMarginalsC() const {
-      if (epReprPrec==0)
+      if (epRepr->numPrecVariables()==0)
 	throw WrongStatusException(EXCEPT_MSG(""));
       return margC;
     }
@@ -272,45 +261,36 @@
   {
     int i,ii,vjSz,k;
     double temp,temp2,cH,cRho,bval,nu,alpha,cPi,cBeta,pi,beta,tilPi,tilBeta,
-      prPi,prBeta,kappa,thres2=0.5*piMinThres,mH,mRho,cA,cC,hatA,hatC,prA,prC;
+      prPi,prBeta,kappa,thres2=0.5*piMinThres,mH,mRho,cA,cC,hatA,hatC,prA,prC,
+      mnTau,stdTau;
     const int* vjInd;
     const double* bP;
     double* betaP,*piP,*cBetaP,*cPiP,*mBetaP,*mPiP,*mprBetaP,*mprPiP,*aP,*cP;
     double inp[4],ret[4];
-    bool hasBVPrec=(epReprPrec!=0);
+    bool isBVPrec=(epPots->getPot(j).getArgumentGroup()==
+		   EPScalarPotential::atypeBivarPrec);
     char debMsg[200]; // DEBUG!
 
-    MYASS((!hasBVPrec && epPots->getPot(j).getArgumentGroup()==
-	   EPScalarPotential::atypeUnivariate) ||
-	  (hasBVPrec && epPots->getPot(j).getArgumentGroup()==
-	   EPScalarPotential::atypeBivarPrec));
     if (dampFact<0.0 || dampFact>=1.0)
       throw InvalidParameterException(EXCEPT_MSG(""));
     // Access to data for j. Temporary arrays
-    //printMsgStdout("epRepr->accessRow");
     epRepr->accessRow(j,vjSz,vjInd,bP,betaP,piP);
-    //printMsgStdout("OK");
-    if (hasBVPrec)
-      k=epReprPrec->accessTauRow(j,aP,cP); // k=k(j)
-    mBetaP=margBeta.p(); mPiP=margPi.p();
-    if (buffVec.size()<4*vjSz) {
-      //sprintf(debMsg,"buffVec.changeRep: %d",4*vjSz);
-      //printMsgStdout(debMsg);
-      buffVec.changeRep(4*vjSz);
-      //printMsgStdout("OK");
+    if (isBVPrec) {
+      k=epRepr->accessTauRow(j,aP,cP); // k=k(j)
+      mnTau=margA[k]/margC[k]; // For '*delta' below
+      stdTau=sqrt(margA[k])/margC[k];
     }
+    mBetaP=margBeta.p(); mPiP=margPi.p();
+    if (buffVec.size()<4*vjSz)
+      buffVec.changeRep(4*vjSz);
     cBetaP=buffVec.p(); cPiP=cBetaP+vjSz;
     mprBetaP=cPiP+vjSz; mprPiP=mprBetaP+vjSz;
     // Compute cavity marginals
     // The marginal moments on s_j ('mH', 'mRho') are required to compute
     // '*delta' below
-    //printMsgStdout("sequentialUpdate: Compute cavities"); // DEBUG!
     cH=cRho=mH=mRho=0.0;
     for (ii=0; ii<vjSz; ii++) {
       i=vjInd[ii];
-      //sprintf(debMsg,"ii=%d,i=%d,mPi=%f,mBeta=%f,pi=%f,beta=%f",ii,i,
-      //      mPiP[i],mBetaP[i],piP[ii],betaP[ii]); // DEBUG!
-      //printMsgStdout(debMsg);
       if ((cPiP[ii]=cPi=mPiP[i]-piP[ii])<thres2)
 	return updCavityInvalid; // EP update failed
       cBetaP[ii]=cBeta=mBetaP[i]-betaP[ii];
@@ -321,22 +301,20 @@
       mRho+=bval*temp;
       mH+=temp*mBetaP[i];
     }
-    if (hasBVPrec) {
+    if (isBVPrec) {
       if ((cA=margA[k]-(*aP))<0.5*aMinThres)
 	return updCavityInvalid; // EP update failed
       if ((cC=margC[k]-(*cP))<0.5*cMinThres)
 	return updCavityInvalid; // EP update failed
     }
-    //sprintf(debMsg,"Cav. marg.: cH=%f,cRho=%f",cH,cRho); // DEBUG!
-    //printMsgStdout(debMsg);
     // Local EP update
     inp[0]=cH; inp[1]=cRho;
-    if (hasBVPrec) {
+    if (isBVPrec) {
       inp[2]=cA; inp[3]=cC;
     }
     if (!epPots->getPot(j).compMoments(inp,ret)) {
       // DEBUG:
-      if (!hasBVPrec)
+      if (!isBVPrec)
 	sprintf(debMsg,"UUPS: j=%d, cH=%f,cRho=%f",j,cH,cRho);
       else
 	sprintf(debMsg,"UUPS: j=%d, cH=%f,cRho=%f,cA=%f,cC=%f",j,cH,cRho,cA,
@@ -345,23 +323,19 @@
       return updNumericalError; // EP update failed
     }
     alpha=ret[0]; nu=ret[1];
-    if (hasBVPrec) {
+    if (isBVPrec) {
       // New marginal a, c parameters (without damping)
       hatA=ret[2]; hatC=ret[3];
     }
     // Compute new EP parameters without damping (to 'mprXXP'). If
     // selective damping is active, we also determine the effective damping
     // factor (overwrites 'dampFact')
-    //printMsgStdout("sequentialUpdate: 2"); // DEBUG!
     for (ii=0; ii<vjSz; ii++) {
       // Undamped EP update
       i=vjInd[ii];
       bval=bP[ii]; // b_{ji}
       pi=piP[ii]; beta=betaP[ii]; // pi_{ji}, beta_{ji}
       cPi=cPiP[ii]; cBeta=cBetaP[ii]; // pi_{-ji}, beta_{-ji}
-      //sprintf(debMsg,"ii=%d,i=%d,bval=%f,cPi=%f,cBeta=%f",ii,i,bval,cPi,
-      //      cBeta); // DEBUG!
-      //printMsgStdout(debMsg);
       // 'tilPi', 'tilBeta': tilde{pi}_{ji}, tilde{beta}_{ji}, EP updates
       // without damping
       if (fabs(bval)>1e-6) {
@@ -426,7 +400,7 @@
 	dampFact=std::max(dampFact,eta);
       }
     }
-    if (hasBVPrec) {
+    if (isBVPrec) {
       // Selective damping
       prA=hatA-cA; prC=hatC-cC;
       if (!(epMaxA==0) && prA<*aP) {
@@ -504,8 +478,6 @@
       pi=piP[ii]; beta=betaP[ii]; // Current parameters
       cPi=cPiP[ii]; cBeta=cBetaP[ii]; // Cavity
       prPi=mprPiP[ii]; prBeta=mprBetaP[ii]; // Undamped update
-      //sprintf(debMsg,"ii=%d,prPi=%f,prBeta=%f",ii,prPi,prBeta); // DEBUG!
-      //printMsgStdout(debMsg);
       // Damping
       if (dampFact>0.0) {
 	prPi+=dampFact*(pi-prPi);
@@ -519,7 +491,7 @@
       cPiP[ii]=prPi; cBetaP[ii]=prBeta; // New EP parameters
     }
     // New EP parameters and marginals for Gamma parameters: Write back
-    if (hasBVPrec) {
+    if (isBVPrec) {
       prA=hatA-cA; prC=hatC-cC;
       if (dampFact>0.0) {
 	prA+=dampFact*(*aP-prA);
@@ -533,16 +505,11 @@
 	epMaxA->update(k,j,prA);
       if (!(epMaxC==0))
 	epMaxC->update(k,j,prC);
-      // HIER: '*delta'!!
-      // Need mean and stddev. of Gamma!
     }
     // Update succeeded: Write back new EP parameters and marginals
     double mprH=0.0,mprRho=0.0; // For '*delta'
     for (ii=0; ii<vjSz; ii++) {
       i=vjInd[ii];
-      //sprintf(debMsg,"ii=%d,i=%d,beta'=%f,pi'=%f,mbeta'=%f,mpi'=%f",ii,i,
-      //      cBetaP[ii],cPiP[ii],mprBetaP[ii],mprPiP[ii]); // DEBUG!
-      //printMsgStdout(debMsg);
       betaP[ii]=cBetaP[ii]; piP[ii]=cPiP[ii]; // New EP pars
       mBetaP[i]=mprBetaP[ii]; mPiP[i]=mprPiP[ii]; // New marginals
       // For '*delta':
@@ -555,6 +522,11 @@
     if (delta!=0) {
       mRho=sqrt(mRho); mprRho=sqrt(mprRho);
       *delta=std::max(MAXRELDIFF(mH,mprH),MAXRELDIFF(mRho,mprRho));
+      if (isBVPrec) {
+	temp=margA[k]/margC[k]; temp2=sqrt(margA[k])/margC[k];
+	*delta=std::max(*delta,MAXRELDIFF(mnTau,temp));
+	*delta=std::max(*delta,MAXRELDIFF(stdTau,temp2));
+      }
     }
 
     return updSuccess;
