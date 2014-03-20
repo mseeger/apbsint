295f73c4726e12d191106fc92691b3c6c8fe1f11
Matthias Seeger <mseeger@gmail.com>
2014-03-20 19:05:43 +0100

    New Cython wrappers (not finished)

--- python/cython/eptools_ext.pyx
+++ python/cython/eptools_ext.pyx
@@ -55,9 +55,12 @@ def epupdate_parallel(np.ndarray[int,ndim=1] potids not None,
                       np.ndarray[np.double_t,ndim=1] nu not None,
                       np.ndarray[np.double_t,ndim=1] logz = None,
                       np.ndarray[int,ndim=1] updind = None):
-    cdef int rsz, errcode
+    cdef int rsz, errcode, ain, aout
     cdef char errstr[512]
     cdef void** annobj_p
+    cdef int logz_n, updind_n
+    cdef double* logz_p
+    cdef int* updind_p
     # Ensure that input/output arguments are contiguous
     rsz = cmu.shape[0]
     if not (rstat.flags.c_contiguous and rstat.shape[0]==rsz):
@@ -84,48 +87,111 @@ def epupdate_parallel(np.ndarray[int,ndim=1] potids not None,
     #cdef np.ndarray[np.double_t,ndim=1] logz = np.zeros(rsz,dtype=np.double)
     # Call C function
     if updind is None:
-        if logz is None:
-            eptwrap_epupdate_parallel(7,3,&potids[0],potids.shape[0],&numpot[0],
-                                      numpot.shape[0],&parvec[0],
-                                      parvec.shape[0],&parshrd[0],
-                                      parshrd.shape[0],annobj_p,annobj.shape[0],
-                                      &cmu[0],cmu.shape[0],&crho[0],
-                                      crho.shape[0],NULL,0,&rstat[0],
-                                      rstat.shape[0],&alpha[0],alpha.shape[0],
-                                      &nu[0],nu.shape[0],NULL,0,&errcode,errstr)
-        else:
-            eptwrap_epupdate_parallel(7,4,&potids[0],potids.shape[0],&numpot[0],
-                                      numpot.shape[0],&parvec[0],
-                                      parvec.shape[0],&parshrd[0],
-                                      parshrd.shape[0],annobj_p,
-                                      annobj.shape[0],&cmu[0],cmu.shape[0],
-                                      &crho[0],crho.shape[0],NULL,0,&rstat[0],
-                                      rstat.shape[0],&alpha[0],alpha.shape[0],
-                                      &nu[0],nu.shape[0],&logz[0],
-                                      logz.shape[0],&errcode,errstr)
+        updind_n = 0
+        updind_p = NULL
+        ain = 7
     else:
         updind = np.ascontiguousarray(updind)
-        if logz is None:
-            eptwrap_epupdate_parallel(8,3,&potids[0],potids.shape[0],&numpot[0],
-                                      numpot.shape[0],&parvec[0],
-                                      parvec.shape[0],&parshrd[0],
-                                      parshrd.shape[0],annobj_p,
-                                      annobj.shape[0],&cmu[0],cmu.shape[0],
-                                      &crho[0],crho.shape[0],&updind[0],
-                                      updind.shape[0],&rstat[0],rstat.shape[0],
-                                      &alpha[0],alpha.shape[0],&nu[0],
-                                      nu.shape[0],NULL,0,&errcode,errstr)
-        else:
-            eptwrap_epupdate_parallel(8,4,&potids[0],potids.shape[0],&numpot[0],
-                                      numpot.shape[0],&parvec[0],
-                                      parvec.shape[0],&parshrd[0],
-                                      parshrd.shape[0],annobj_p,
-                                      annobj.shape[0],&cmu[0],cmu.shape[0],
-                                      &crho[0],crho.shape[0],&updind[0],
-                                      updind.shape[0],&rstat[0],rstat.shape[0],
-                                      &alpha[0],alpha.shape[0],&nu[0],
-                                      nu.shape[0],&logz[0],logz.shape[0],
-                                      &errcode,errstr)
+        updind_n = updind.shape[0]
+        updind_p = &updind[0]
+        ain = 8
+    if logz is None:
+        logz_n = 0
+        logz_p = NULL
+        aout = 3
+    else:
+        logz_n = logz.shape[0]
+        logz_p = &logz[0]
+        aout = 4
+    eptwrap_epupdate_parallel(ain,aout,&potids[0],potids.shape[0],&numpot[0],
+                              numpot.shape[0],&parvec[0],parvec.shape[0],
+                              &parshrd[0],parshrd.shape[0],annobj_p,
+                              annobj.shape[0],&cmu[0],cmu.shape[0],&crho[0],
+                              crho.shape[0],updind_p,updind_n,&rstat[0],
+                              rstat.shape[0],&alpha[0],alpha.shape[0],&nu[0],
+                              nu.shape[0],logz_p,logz_n,&errcode,errstr)
+    PyMem_Free(annobj_p)  # Free temp. void* array
+    # Check for error, raise exception
+    if errcode != 0:
+        raise exc.ApBsWrapError(<bytes>errstr)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def epupdate_parallel_bvprec(np.ndarray[int,ndim=1] potids not None,
+                             np.ndarray[int,ndim=1] numpot not None,
+                             np.ndarray[np.double_t,ndim=1] parvec not None,
+                             np.ndarray[int,ndim=1] parshrd not None,
+                             np.ndarray[np.uint64_t,ndim=1] annobj not None,
+                             np.ndarray[np.double_t,ndim=1] cmu not None,
+                             np.ndarray[np.double_t,ndim=1] crho not None,
+                             np.ndarray[np.double_t,ndim=1] ca not None,
+                             np.ndarray[np.double_t,ndim=1] cc not None,
+                             np.ndarray[int,ndim=1] rstat not None,
+                             np.ndarray[np.double_t,ndim=1] alpha not None,
+                             np.ndarray[np.double_t,ndim=1] nu not None,
+                             np.ndarray[np.double_t,ndim=1] hata not None,
+                             np.ndarray[np.double_t,ndim=1] hatc not None,
+                             np.ndarray[np.double_t,ndim=1] logz = None,
+                             np.ndarray[int,ndim=1] updind = None):
+    cdef int rsz, errcode, ain, aout
+    cdef char errstr[512]
+    cdef void** annobj_p
+    cdef int logz_n, updind_n
+    cdef double* logz_p
+    cdef int* updind_p
+    # Ensure that input/output arguments are contiguous
+    rsz = cmu.shape[0]
+    if not (rstat.flags.c_contiguous and rstat.shape[0]==rsz):
+        raise TypeError('RSTAT must be contiguous array of size of CMU')
+    if not (alpha.flags.c_contiguous and alpha.shape[0]==rsz):
+        raise TypeError('ALPHA must be contiguous array of size of CMU')
+    if not (nu.flags.c_contiguous and nu.shape[0]==rsz):
+        raise TypeError('NU must be contiguous array of size of CMU')
+    if not (logz is None or (logz.flags.c_contiguous and logz.shape[0]==rsz)):
+        raise TypeError('LOGZ must be contiguous array of size of CMU')
+    rsz = ca.shape[0]
+    if not (hata.flags.c_contiguous and hata.shape[0]==rsz):
+        raise TypeError('HATA must be contiguous array of size of CA')
+    if not (hatc.flags.c_contiguous and hatc.shape[0]==rsz):
+        raise TypeError('HATC must be contiguous array of size of CA')
+    potids = np.ascontiguousarray(potids)
+    numpot = np.ascontiguousarray(numpot)
+    parvec = np.ascontiguousarray(parvec)
+    parshrd = np.ascontiguousarray(parshrd)
+    cmu = np.ascontiguousarray(cmu)
+    crho = np.ascontiguousarray(crho)
+    ca = np.ascontiguousarray(ca)
+    cc = np.ascontiguousarray(cc)
+    annobj_p = make_voidptr_array(annobj)  # Convert to void* array
+    # Call C function
+    if updind is None:
+        updind_n = 0
+        updind_p = NULL
+        ain = 9
+    else:
+        updind = np.ascontiguousarray(updind)
+        updind_n = updind.shape[0]
+        updind_p = &updind[0]
+        ain = 10
+    if logz is None:
+        logz_n = 0
+        logz_p = NULL
+        aout = 5
+    else:
+        logz_n = logz.shape[0]
+        logz_p = &logz[0]
+        aout = 6
+    eptwrap_epupdate_parallel_bvprec(ain,aout,&potids[0],potids.shape[0],
+                                     &numpot[0],numpot.shape[0],&parvec[0],
+                                     parvec.shape[0],&parshrd[0],
+                                     parshrd.shape[0],annobj_p,annobj.shape[0],
+                                     &cmu[0],cmu.shape[0],&crho[0],
+                                     crho.shape[0],&ca[0],ca.shape[0],&cc[0],
+                                     cc.shape[0],updind_p,updind_n,&rstat[0],
+                                     rstat.shape[0],&alpha[0],alpha.shape[0],
+                                     &nu[0],nu.shape[0],&hata[0],hata.shape[0],
+                                     &hatc[0],hatc.shape[0],logz_p,logz_n,
+                                     &errcode,errstr)
     PyMem_Free(annobj_p)  # Free temp. void* array
     # Check for error, raise exception
     if errcode != 0:
@@ -151,6 +217,16 @@ def getpotname(int pid):
     if errcode != 0:
         raise exc.ApBsWrapError(<bytes>errstr)
     return <bytes>name
+
+def getpotagroup(int pid):
+    cdef int errcode, agid
+    cdef char errstr[512]
+    # Call C function
+    eptwrap_getpotagroup(1,1,pid,&agid,&errcode,errstr)
+    # Check for error, raise exception
+    if errcode != 0:
+        raise exc.ApBsWrapError(<bytes>errstr)
+    return agid
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -186,6 +262,56 @@ def fact_compmarginals(int n,int m,np.ndarray[int,ndim=1] rp_rowind not None,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
+def fact_compmarginals_bvprec(int n,int m,
+                              np.ndarray[int,ndim=1] rp_rowind not None,
+                              np.ndarray[int,ndim=1] rp_colind not None,
+                              np.ndarray[np.double_t,ndim=1] rp_bvals not None,
+                              np.ndarray[np.double_t,ndim=1] rp_pi not None,
+                              np.ndarray[np.double_t,ndim=1] rp_beta not None,
+                              np.ndarray[int,ndim=1] rp_tauind not None,
+                              np.ndarray[np.double_t,ndim=1] rp_a not None,
+                              np.ndarray[np.double_t,ndim=1] rp_c not None,
+                              np.ndarray[np.double_t,ndim=1] margpi not None,
+                              np.ndarray[np.double_t,ndim=1] margbeta not None,
+                              np.ndarray[np.double_t,ndim=1] marga not None,
+                              np.ndarray[np.double_t,ndim=1] margc not None):
+    cdef int errcode
+    cdef char errstr[512]
+    # Ensure that input/output arguments are contiguous
+    if not margpi.flags.c_contiguous:
+        raise TypeError('MARGPI must be contiguous array')
+    if not margbeta.flags.c_contiguous:
+        raise TypeError('MARGBETA must be contiguous array')
+    if not marga.flags.c_contiguous:
+        raise TypeError('MARGA must be contiguous array')
+    if not margc.flags.c_contiguous:
+        raise TypeError('MARGC must be contiguous array')
+    rp_rowind = np.ascontiguousarray(rp_rowind)
+    rp_colind = np.ascontiguousarray(rp_colind)
+    rp_bvals = np.ascontiguousarray(rp_bvals)
+    rp_pi = np.ascontiguousarray(rp_pi)
+    rp_beta = np.ascontiguousarray(rp_beta)
+    rp_tauind = np.ascontiguousarray(rp_tauind)
+    rp_a = np.ascontiguousarray(rp_a)
+    rp_c = np.ascontiguousarray(rp_c)
+    # Call C function
+    eptwrap_fact_compmarginals_bvprec(15,0,n,m,&rp_rowind[0],rp_rowind.shape[0],
+                                      &rp_colind[0],rp_colind.shape[0],
+                                      &rp_bvals[0],rp_bvals.shape[0],&rp_pi[0],
+                                      rp_pi.shape[0],&rp_beta[0],
+                                      rp_beta.shape[0],&rp_tauind[0],
+                                      rp_tauind.shape[0],&rp_a[0],
+                                      rp_a.shape[0],&rp_c[0],rp_c.shape[0],
+                                      &margpi[0],margpi.shape[0],&margbeta[0],
+                                      margbeta.shape[0],&marga[0],
+                                      marga.shape[0],&margc[0],margc.shape[0],
+                                      &errcode,errstr)
+    # Check for error, raise exception
+    if errcode != 0:
+        raise exc.ApBsWrapError(<bytes>errstr)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def fact_compmaxpi(int n,int m,np.ndarray[int,ndim=1] rp_rowind not None,
                    np.ndarray[int,ndim=1] rp_colind not None,
                    np.ndarray[np.double_t,ndim=1] rp_bvals not None,
@@ -193,8 +319,9 @@ def fact_compmaxpi(int n,int m,np.ndarray[int,ndim=1] rp_rowind not None,
                    np.ndarray[np.double_t,ndim=1] rp_beta not None,
                    int sd_k,np.ndarray[int,ndim=1] sd_subind = None,
                    int sd_subexcl = 0):
-    cdef int errcode, rsz
+    cdef int errcode, rsz, subind_n, ain
     cdef char errstr[512]
+    cdef int* subind_p
     # Ensure that input arguments are contiguous
     rp_rowind = np.ascontiguousarray(rp_rowind)
     rp_colind = np.ascontiguousarray(rp_colind)
@@ -213,27 +340,84 @@ def fact_compmaxpi(int n,int m,np.ndarray[int,ndim=1] rp_rowind not None,
         np.zeros(rsz,dtype=np.double)
     # Call C function
     if sd_subind is None:
-        eptwrap_fact_compmaxpi(8,3,n,m,&rp_rowind[0],rp_rowind.shape[0],
-                               &rp_colind[0],rp_colind.shape[0],&rp_bvals[0],
-                               rp_bvals.shape[0],&rp_pi[0],rp_pi.shape[0],
-                               &rp_beta[0],rp_beta.shape[0],sd_k,NULL,0,
-                               sd_subexcl,&sd_numvalid[0],sd_numvalid.shape[0],
-                               &sd_topind[0],sd_topind.shape[0],&sd_topval[0],
-                               sd_topval.shape[0],&errcode,errstr)
+        subind_n = 0
+        subind_p = NULL
+        ain = 8
     else:
         sd_subind = np.ascontiguousarray(sd_subind)
-        eptwrap_fact_compmaxpi(10,3,n,m,&rp_rowind[0],rp_rowind.shape[0],
-                               &rp_colind[0],rp_colind.shape[0],&rp_bvals[0],
-                               rp_bvals.shape[0],&rp_pi[0],rp_pi.shape[0],
-                               &rp_beta[0],rp_beta.shape[0],sd_k,&sd_subind[0],
-                               sd_subind.shape[0],sd_subexcl,&sd_numvalid[0],
-                               sd_numvalid.shape[0],&sd_topind[0],
-                               sd_topind.shape[0],&sd_topval[0],
-                               sd_topval.shape[0],&errcode,errstr)
+        subind_n = sd_subind.shape[0]
+        subind_p = &sd_subind[0]
+        ain = 10
+    eptwrap_fact_compmaxpi(ain,3,n,m,&rp_rowind[0],rp_rowind.shape[0],
+                           &rp_colind[0],rp_colind.shape[0],&rp_bvals[0],
+                           rp_bvals.shape[0],&rp_pi[0],rp_pi.shape[0],
+                           &rp_beta[0],rp_beta.shape[0],sd_k,subind_p,subind_n,
+                           sd_subexcl,&sd_numvalid[0],sd_numvalid.shape[0],
+                           &sd_topind[0],sd_topind.shape[0],&sd_topval[0],
+                           sd_topval.shape[0],&errcode,errstr)
     # Check for error, raise exception
     if errcode != 0:
         raise exc.ApBsWrapError(<bytes>errstr)
     return (sd_numvalid,sd_topind,sd_topval)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def fact_compmaxac(int n,int m,int numk,
+                   np.ndarray[int,ndim=1] rp_rowind not None,
+                   np.ndarray[int,ndim=1] rp_colind not None,
+                   np.ndarray[np.double_t,ndim=1] rp_bvals not None,
+                   np.ndarray[np.double_t,ndim=1] rp_pi not None,
+                   np.ndarray[np.double_t,ndim=1] rp_beta not None,
+                   np.ndarray[int,ndim=1] rp_tauind not None,
+                   np.ndarray[np.double_t,ndim=1] rp_a not None,
+                   np.ndarray[np.double_t,ndim=1] rp_c not None,
+                   int sda_k,int sdc_k):
+    cdef int errcode, rsz
+    cdef char errstr[512]
+    # Ensure that input arguments are contiguous
+    rp_rowind = np.ascontiguousarray(rp_rowind)
+    rp_colind = np.ascontiguousarray(rp_colind)
+    rp_bvals = np.ascontiguousarray(rp_bvals)
+    rp_pi = np.ascontiguousarray(rp_pi)
+    rp_beta = np.ascontiguousarray(rp_beta)
+    rp_tauind = np.ascontiguousarray(rp_tauind)
+    rp_a = np.ascontiguousarray(rp_a)
+    rp_c = np.ascontiguousarray(rp_c)
+    # Create return arguments
+    if sda_k<2:
+        raise ValueError('SDA_K: Must be >1')
+    if sdc_k<2:
+        raise ValueError('SDC_K: Must be >1')
+    if numk<1:
+        raise ValueError('NUMK: Must be positive')
+    rsz = numk*(sda_k+1)
+    cdef np.ndarray[int,ndim=1] sda_numvalid = np.zeros(numk,dtype=np.int32)
+    cdef np.ndarray[int,ndim=1] sda_topind = np.zeros(rsz,dtype=np.int32)
+    cdef np.ndarray[np.double_t,ndim=1] sda_topval = \
+        np.zeros(rsz,dtype=np.double)
+    rsz = numk*(sdc_k+1)
+    cdef np.ndarray[int,ndim=1] sdc_numvalid = np.zeros(numk,dtype=np.int32)
+    cdef np.ndarray[int,ndim=1] sdc_topind = np.zeros(rsz,dtype=np.int32)
+    cdef np.ndarray[np.double_t,ndim=1] sdc_topval = \
+        np.zeros(rsz,dtype=np.double)
+    # Call C function
+    eptwrap_fact_compmaxac(12,6,n,m,&rp_rowind[0],rp_rowind.shape[0],
+                           &rp_colind[0],rp_colind.shape[0],&rp_bvals[0],
+                           rp_bvals.shape[0],&rp_pi[0],rp_pi.shape[0],
+                           &rp_beta[0],rp_beta.shape[0],&rp_tauind[0],
+                           rp_tauind.shape[0],&rp_a[0],rp_a.shape[0],&rp_c[0],
+                           rp_c.shape[0],sda_k,sdc_k,&sda_numvalid[0],
+                           sda_numvalid.shape[0],&sda_topind[0],
+                           sda_topind.shape[0],&sda_topval[0],
+                           sda_topval.shape[0],&sdc_numvalid[0],
+                           sdc_numvalid.shape[0],&sdc_topind[0],
+                           sdc_topind.shape[0],&sdc_topval[0],
+                           sdc_topval.shape[0],&errcode,errstr)
+    # Check for error, raise exception
+    if errcode != 0:
+        raise exc.ApBsWrapError(<bytes>errstr)
+    return (sda_numvalid,sda_topind,sda_topval,sdc_numvalid,sdc_topind,
+            sdc_topval)
 
 # NOTE: sd_nupd, sd_nrec are returned only if rstat, delta, sd_dampfact and
 # sd_numvalid are all given
